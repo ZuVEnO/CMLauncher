8753fb4088867ea8e13d54817fce35d3d3534417
ZuVEnO <ytyca1@gmail.com>
2021-04-17 12:19:29 +0300

    First developing on C#

---
+++ CMLauncher/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace CMLauncher
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("Loading...");
+            // Загрузка... 1000 ms - 1 секунда
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Test");
+
+            ConsoleKey consoleKey = Console.ReadKey().Key;
+
+            if (consoleKey == ConsoleKey.Enter)
+            {
+                Console.Clear();
+                Console.Write("Hello");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.Clear();
+                Console.Write("Error");
+            }
+        }
+    }
+}
