f2425c3a2133939bb2b6726d08967d3439417c90
ZuVEnO <ytyca1@gmail.com>
2021-04-18 12:41:11 +0300

    Create

--- CMLauncher/Program.cs
+++ CMLauncher/Program.cs
@@ -104,7 +104,6 @@ namespace CMLauncher
                         Console.Write("GitHub: ");
                         Console.ForegroundColor = ConsoleColor.Blue;
                         Console.Write("https://www.github.com/ZuVEnO/CMLauncher");
-                        Console.ReadKey(true);
                         ConsoleKey consoleKey = Console.ReadKey().Key;
 
                         if (consoleKey == ConsoleKey.Oem1 | consoleKey == ConsoleKey.NumPad1 | consoleKey == ConsoleKey.D1)
@@ -135,57 +134,106 @@ namespace CMLauncher
                 Console.WriteLine("Все сервера создаются в папке на Рабочем Столе.");
                 Console.WriteLine("Укажите название вашего сервера: ");
                 String folderName = Console.ReadLine();
+                SelectVer();
                 // 1 Стадия: Название папки сервера
-                Console.Clear();
-                Console.Title = "CMLauncher " + status + "-" + ver + " | Создание сервера 2/3";
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.Write("CMLauncher ");
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.Write(" | ");
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write("Создание сервера 2/3");
-                Console.WriteLine();
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("Здесь показан список версии CraftBukkit:");
-                Console.WriteLine("1.16.5 | 1.16.4 | 1.16.3 | 1.16.2 | 1.16.1");
-                Console.WriteLine("1.15.2 | 1.15.1 | 1.15 | 1.14.4 | 1.14.3");
-                Console.WriteLine("1.14.2 | 1.14.1 | 1.14 | 1.13.2 | 1.13.1");
-                Console.WriteLine("1.13 | 1.12.2 | 1.12.1 | 1.12 | 1.11.2");
-                Console.WriteLine("1.11.1 | 1.11 | 1.10.2 | 1.10.1 | 1.10");
-                Console.WriteLine("1.9.4 | 1.9.2 | 1.9 | 1.8.8 | 1.8.7 | 1.8.6");
-                Console.WriteLine("1.8.5| 1.8.4 | 1.8.3 | 1.8.2 | 1.8.1 | 1.8");
-                Console.WriteLine("");
-                Console.WriteLine("Укажите версию");
-                String version = Console.ReadLine();
-                if(version.Equals("1.16.5") | version.Equals("1.16.4") | version.Equals("1.16.3"))
-                StartCreate();
-
-                void StartCreate()
+                void SelectVer()
                 {
                     Console.Clear();
-                    Console.WriteLine("Создание папки...");
-                    Directory.CreateDirectory(path: "C:/Users/" + Environment.UserName + "/Desktop/Server Projets/" + folderName);
-                    Console.WriteLine("Скачивание ядра с хостинга... Потребуется время...");
-                    web.DownloadFileCompleted += new System.ComponentModel.AsyncCompletedEventHandler(Completed);
-                    if()
-                    web.DownloadFileAsync(new Uri("https://cdn.getbukkit.org/craftbukkit/craftbukkit-1.16.5.jar"), "C:/Users/" + Environment.UserName + "/Desktop/Server Projets/" + folderName + "/" + "craftbukkit-1.16.5.jar");
-                    Console.ReadKey(true);
+                    Console.Title = "CMLauncher " + status + "-" + ver + " | Создание сервера 2/3";
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write("CMLauncher ");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write(" | ");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("Создание сервера 2/3");
+                    Console.WriteLine();
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Здесь показан список версии CraftBukkit:");
+                    Console.WriteLine("1.16.5 | 1.16.4 | 1.16.3 | 1.16.2 | 1.16.1");
+                    Console.WriteLine("1.15.2 | 1.15.1 | 1.15 | 1.14.4 | 1.14.3");
+                    Console.WriteLine("1.14.2 | 1.14.1 | 1.14 | 1.13.2 | 1.13.1");
+                    Console.WriteLine("1.13 | 1.12.2 | 1.12.1 | 1.12 | 1.11.2");
+                    Console.WriteLine("1.11.1 | 1.11 | 1.10.2 | 1.10.1 | 1.10");
+                    Console.WriteLine("1.9.4 | 1.9.2 | 1.9 | 1.8.8 | 1.8.7 | 1.8.6");
+                    Console.WriteLine("1.8.5| 1.8.4 | 1.8.3 | 1.8.2 | 1.8.1 | 1.8");
+                    Console.WriteLine("");
+                    Console.WriteLine("Укажите версию");
+                    String version = Console.ReadLine();
+                    if (version.Equals("1.16.5") | version.Equals("1.16.4") | version.Equals("1.16.3") | version.Equals("1.16.2") | version.Equals("1.16.1")
+                         | version.Equals("1.15.2") | version.Equals("1.15.1") | version.Equals("1.15") | version.Equals("1.14.4") | version.Equals("1.14.3")
+                          | version.Equals("1.14.2") | version.Equals("1.14.1") | version.Equals("1.14") | version.Equals("1.13.2") | version.Equals("1.13.1")
+                           | version.Equals("1.13") | version.Equals("1.12.2") | version.Equals("1.12.1") | version.Equals("1.12") | version.Equals("1.11.2")
+                            | version.Equals("1.11.1") | version.Equals("1.11") | version.Equals("1.10.2") | version.Equals("1.10") | version.Equals("1.9.4")
+                             | version.Equals("1.9.2") | version.Equals("1.9") | version.Equals("1.8.8") | version.Equals("1.8.7") | version.Equals("1.8.6")
+                             | version.Equals("1.8.5") | version.Equals("1.8.4") | version.Equals("1.8.3") | version.Equals("1.8.2") | version.Equals("1.8.1") | version.Equals("1.8"))
+                        StartCreate();
+                    else
+                    {
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.WriteLine("ERROR!");
+                        Console.WriteLine("Версия не найдена!");
+                        Console.ReadKey(true);
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        SelectVer();
+                    }
+
+                    void StartCreate()
+                    {
+                        Console.Clear();
+                        Console.Title = "CMLauncher " + status + "-" + ver + " | Создание сервера...";
+                        Console.WriteLine("Создание папки...");
+                        Directory.CreateDirectory(path: "C:/Users/" + Environment.UserName + "/Desktop/Server Projets/" + folderName);
+                        Console.WriteLine("Скачивание ядра с хостинга... Потребуется время...");
+                        web.DownloadFileCompleted += new System.ComponentModel.AsyncCompletedEventHandler(Completed);
+                        web.DownloadProgressChanged += new DownloadProgressChangedEventHandler(Progress);
+                        web.DownloadFileAsync(new Uri("https://cdn.getbukkit.org/craftbukkit/craftbukkit-" + version + ".jar"), "C:/Users/" + Environment.UserName + "/Desktop/Server Projets/" + folderName + "/" + "craftbukkit-1.16.5.jar");
+                        Console.ReadKey(true);
+                        void next2() {
+                            Console.WriteLine("Приписываем параметры и подтверждаем eula.txt...");
+                            var eula = new StreamWriter("C:/Users/" + Environment.UserName + "/Desktop/Server Projets/" + folderName + "/" + "eula.txt");
+                            eula.Write("eula=true");
+                            var serprop = new StreamWriter("C:/Users/" + Environment.UserName + "/Desktop/Server Projets/" + folderName + "/" + "server.properties");
+                            serprop.WriteLine("online-mode=false");
+                            serprop.WriteLine("motd=\u00a7aThanks for using CML ;)");
+                            Console.WriteLine("Сервер создан!");
+                            Console.WriteLine("Нажмите на любую кнопку, чтобы выйти");
+                            Console.ReadKey(true);
+                            return;
+                        }
+                        
+
+                        void Completed(Object sender, AsyncCompletedEventArgs e)
+                        {
+                            if (e.Error != null)
+                            {
+                                Console.WriteLine("Ошибка при скачивании: " + e.Error.Message);
+                                Console.WriteLine("Создание отменено. Нажмите на любую кнопку, чтобы выйти");
+                                Console.ReadKey(true);
+                                return;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Ядро скачано");
+                                Thread.Sleep(100);
+                                next2();
+                            }
+                        }
+
+                        void Progress(Object sender, DownloadProgressChangedEventArgs e)
+                        {
+                            Console.Title = "CMLauncher скачивание: " + e.BytesReceived + " " + e.ProgressPercentage + "%";
+                            if(e.ProgressPercentage.Equals("100"))
+                            {
+                                Console.Title = "CMLauncher " + status + "-" + ver + " | Создание сервера...";
+                                Console.WriteLine("Ядро скачано");
+                                Thread.Sleep(100);
+                                next2();
+                            }
+                        }
+                    }
                 }
             }
-
-            void Completed(Object sender, AsyncCompletedEventArgs e)
-            {
-                if(e.Error != null)
-                {
-                    Console.WriteLine("Ошибка при скачивании: " + e.Error.Message);
-                }
-                else
-                {
-                    Console.WriteLine("Ядро скачано");
-                    Console.ReadKey(true);
-                }
-            }
-
         }
     }
 }
